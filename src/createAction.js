82d2ca35dce7b2904c786ac5b4b9bb715981d775
Ricardo Stuven <rstuven@gmail.com>
2015-10-06 13:13:39 -0300

    Refactor private names

--- src/createAction.js
+++ src/createAction.js
@@ -4,53 +4,53 @@ function identity(value) {
   return value;
 }
 
-function isValidator(input) {
-  return typeof input === 'object' && !(input instanceof Array);
+function isValidator(spec) {
+  return typeof spec === 'object' && !(spec instanceof Array);
 }
 
 function validatedIdentity(schema, name) {
   const validate = validator(schema);
-  return input => {
-    if (validate(input)) {
-      return input;
+  return value => {
+    if (validate(value)) {
+      return value;
     }
     const { field, message } = validate.errors[0];
     return new Error(`${field.replace(/^data/, name)} ${message}`);
   };
 }
 
-function creator(input, name) {
-  if (typeof input === 'boolean') {
-    return input ? identity : undefined;
+function propertyCreator(spec, name) {
+  if (typeof spec === 'boolean') {
+    return spec ? identity : undefined;
   }
-  if (typeof input === 'function') {
-    return input;
+  if (typeof spec === 'function') {
+    return spec;
   }
-  if (typeof input === 'object' && input instanceof Array) {
-    return (data) => input.reduce((res, prop) => {
+  if (typeof spec === 'object' && spec instanceof Array) {
+    return (data) => spec.reduce((res, prop) => {
       res[prop] = data[prop];
       return res;
     }, {});
   }
-  if (isValidator(input)) {
-    return validatedIdentity(input, name);
+  if (isValidator(spec)) {
+    return validatedIdentity(spec, name);
   }
 }
 
-export default function createAction(type, payloadCreator, metaCreator) {
-  const finalPayloadCreator = creator(payloadCreator, 'payload');
-  const finalMetaCreator = creator(metaCreator, 'meta');
+export default function createAction(type, payloadSpec, metaSpec) {
+  const payloadCreator = propertyCreator(payloadSpec, 'payload');
+  const metaCreator = propertyCreator(metaSpec, 'meta');
 
   function actionCreator(payload, meta) {
     const action = { type };
-    if (typeof finalPayloadCreator === 'function') {
-      action.payload = finalPayloadCreator(payload);
+    if (typeof payloadCreator === 'function') {
+      action.payload = payloadCreator(payload);
       if (action.payload instanceof Error) {
         action.error = true;
       }
     }
-    if (typeof finalMetaCreator === 'function') {
-      action.meta = finalMetaCreator(meta);
+    if (typeof metaCreator === 'function') {
+      action.meta = metaCreator(meta);
     }
     return action;
   }
